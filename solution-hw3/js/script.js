5ef75de216160db879cbca0a8d1b6982cb9d504f
zoya-egi <112358997+zoya-egi@users.noreply.github.com>
2022-09-22 14:34:16 -0400

    added comments and cleaned code!

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,4 +1,6 @@
+// class constructor for glaze objects
 class Glaze {
+    // two class properties, the type of glaze (glazeType) and the price adaptation (glazePrice)
     glazeType;
     glazePrice;
 
@@ -8,12 +10,15 @@ class Glaze {
     }
 }
 
+// creating the objects using the Glaze class
 const keepOriginal = new Glaze("Keep original", 0);
 const sugarMilk = new Glaze("Sugar milk", 0);
 const vanillaMilk = new Glaze("Vanilla milk", 0.50);
 const doubleChocolate = new Glaze("Double chocolate", 1.50);
 
+// class constructor for Pack objects
 class Packs {
+    // two class properties, the size of the pack (packSize), and the price adaptation (packPrice)
     packSize;
     packPrice;
 
@@ -23,15 +28,17 @@ class Packs {
     }
 }
 
+// creating the objects using the Packs class
 const onePack = new Packs(1, 1);
 const threePack = new Packs(3, 3);
 const sixPack = new Packs(6, 5);
 const twelvePack = new Packs(12, 10);
 
+// creating arrays that store objects of Glaze and Packs, respectively
 let allGlazes = [keepOriginal, sugarMilk, vanillaMilk, doubleChocolate];
 let allPacks = [onePack, threePack, sixPack, twelvePack];
 
-// loops
+// a loop to dynamicall populate the glaze dropdown menu
 let glazeOptions = document.getElementById("glazeDropdown");
 for (i = 0; i < allGlazes.length; i++) {
     let glazeOptionItem = document.createElement("option");
@@ -39,6 +46,7 @@ for (i = 0; i < allGlazes.length; i++) {
     glazeOptions.appendChild(glazeOptionItem);
 }
 
+// a loop to dynamicall populate the pack dropdown menu
 let packOptions = document.getElementById("packDropdown");
 for (i = 0; i < allPacks.length; i++) {
     let packOptionItem = document.createElement("option");
@@ -46,33 +54,38 @@ for (i = 0; i < allPacks.length; i++) {
     packOptions.appendChild(packOptionItem);
 }
 
-//console.log(allGlazes[i])
-
+// initialize variables for the base price, glaze price adaptation, pack price adaptation, and the final price
 let basePrice = 2.49;
 let glazeChoice = 0;
 let packChoice = 1;
 let finalPrice;
 
+// calculates the final price 
 function glazingChange(element) {
-    // get value of selected glazing option
+    // this stores the value from the dropdown selection
     const priceChange = element.value;
 
+    // if the value is from the glaze dropdown menu, run through the array that stored all Glaze objects
     for (i = 0; i < allGlazes.length; i++) {
+        // finds the glaze object that matches the one the user selected
         if (allGlazes[i].glazeType == element.value) {
+            // set glazeChoice to the selected glaze object's price adaptation
             glazeChoice = allGlazes[i].glazePrice;
         }
     }
 
+    // if the value is from the pack dropdown menu, run through the array that stored all Packs objects
     for (i = 0; i < allPacks.length; i++) {
+        // finds the pack object that matches the one the user selected
         if (allPacks[i].packSize == element.value) {
+            // set packChoice to the selected pack object's price adaptation
             packChoice = allPacks[i].packPrice;
         }
     }
 
+    // set the finalPrice after performing the math
     finalPrice = ((basePrice + glazeChoice) * packChoice).toFixed(2);
+
+    // dynamically replace the element in html with the finalPrice
     document.getElementById("dynamicPrice").innerHTML = "$ " + finalPrice;
-
-
-
-    // add your code to do update the price ...
 }
