5fb957a9d57dc25f14e2be677aa6aa50df617b0b
zoya-egi <112358997+zoya-egi@users.noreply.github.com>
2022-12-08 20:39:12 -0500

    More animations, and skeleton for content

--- Final Project copy/js/script.js
+++ Final Project copy/js/script.js
@@ -1,28 +1,48 @@
 document.getElementById("openEyes").addEventListener("click", showEyes);
+document.getElementById("shapeShift").addEventListener("click", shapeShifter);
 document.getElementById("toggleBtn").addEventListener("input", auroraON);
 
 let toggleNum = 0;
 
+let eyesImg = document.createElement("img");
+eyesImg.src = "./assets/eyes.gif";
+$(eyesImg).addClass("openEyesGif");
+
+let shapeShifting = document.createElement("div");
+$(shapeShifting).addClass("shifting")
+
 function showEyes() {
     //adding open eye gif
-    let eyesImg = document.createElement("img");
-    eyesImg.src = "./assets/eyes.gif";
-    $(eyesImg).addClass("openEyesGif")
-    document.getElementById("opener2").appendChild(eyesImg);
-    document.getElementById("openEyes").removeEventListener("click", showEyes);
+    if ($(document.getElementById("eyes")).hasClass("bothEyes")) {
+        $(document.getElementById("eyes")).removeClass("bothEyes");
+    }
+    else {
+        $(document.getElementById("shiftShapes")).removeClass("shifting");
+        $(document.getElementById("eyes")).addClass("bothEyes");
+    }
+}
+
+function shapeShifter() {
+    if ($(document.getElementById("shiftShapes")).hasClass("shifting")) {
+        $(document.getElementById("shiftShapes")).removeClass("shifting");
+    }
+    else {
+        $(document.getElementById("eyes")).removeClass("bothEyes");
+        $(document.getElementById("shiftShapes")).addClass("shifting");
+    }
 }
 
 
 function auroraON() {
     toggleNum += 1;
-    if (toggleNum%2 == 1) {
+    if (toggleNum % 2 == 1) {
         document.getElementById("watermark").src = "./assets/auroraON.gif";
-        document.getElementById("hookToTitle").style.zIndex = 9;
+        document.getElementById("hookToTitle").style.zIndex = 1;
     }
     else {
         document.getElementById("watermark").src = "./assets/mountains.png";
     }
-
 }
+
 
 
